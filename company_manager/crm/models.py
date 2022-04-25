1993e0c6e980c39c6757c9ac52da3656c14816d6
Marketa <habranova.marketa@gmail.com>
2022-04-25 19:10:41 +0200

    Added first Signal after creating new user

--- company_manager/crm/models.py
+++ company_manager/crm/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Address(models.Model):
     street = models.CharField(max_length=200, blank=True, null=True)
@@ -57,3 +61,12 @@ class Opportunity(models.Model):
 
     class Meta:
         verbose_name_plural = 'Opportunities'
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Employee.objects.create(user=instance)
