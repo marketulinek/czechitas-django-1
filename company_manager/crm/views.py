6a1fa4b169fc4a25802c0887271131f088f11cf4
Marketa <habranova.marketa@gmail.com>
2022-04-25 17:22:53 +0200

    Added LoginRequired logic for specific pages

--- company_manager/crm/views.py
+++ company_manager/crm/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import CreateView, ListView, TemplateView
 import crm.models as models
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class CompanyCreateView(CreateView):
+class CompanyCreateView(LoginRequiredMixin, CreateView):
     model = models.Company
     template_name = 'company/create_company.html'
     fields = ['name', 'status', 'phone_number', 'email', 'identification_number']
@@ -16,7 +17,7 @@ class CompanyListView(ListView):
     template_name = 'company/list.html'
     fields = ['name', 'status', 'phone_number', 'email', 'address']
 
-class OpportunityCreateView(CreateView):
+class OpportunityCreateView(LoginRequiredMixin, CreateView):
     model = models.Opportunity
     template_name = 'opportunity/create.html'
     fields = ['company', 'sales_manager', 'primary_contact', 'description', 'status']
