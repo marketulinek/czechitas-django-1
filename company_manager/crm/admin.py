ca32b6a8c84e3c061ffa927a5753bf7db8270f2c
Marketa <habranova.marketa@gmail.com>
2022-05-06 20:20:41 +0200

    Customized the admin interface for Company

--- company_manager/crm/admin.py
+++ company_manager/crm/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 import crm.models as models
 
-admin.site.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    fields = ['name', 'phone_number', 'email', 'address', 'status', 'identification_number']
+    readonly_fields = ['status', 'identification_number']
+    list_display = ['name', 'status']
+    list_filter = ['status']
+    search_fields = ['name', 'email', 'identification_number', 'opportunity__description']
+
+admin.site.register(models.Company, CompanyAdmin)
 admin.site.register(models.Opportunity)
 admin.site.register(models.Employee)
