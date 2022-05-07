c6a347cf4e8c893c50465d90777a36d099a8c7e9
Marketa <habranova.marketa@gmail.com>
2022-05-07 19:16:28 +0200

    Added first few automated tests

--- company_manager/crm/tests.py
+++ company_manager/crm/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from crm.models import Company
+
+class CRMViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('tester', 'tester@testing.cz', 'test')
+        Company.objects.create(name='THE MAMA AI', phone_number='123', identification_number='87654321')
+
+    def test_get_company_create(self):
+        self.client.login(username='tester', password='test')
+        response = self.client.get(reverse('company_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_company_create(self):
+        self.client.login(username='tester', password='test')
+        response = self.client.post(reverse('company_create'), data={
+            'name': 'Test Company',
+            'status': 'N',
+            'identification_number': '12345678'
+            }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Company.objects.count(), 2)
+
+    def test_company_list_not_signed(self):
+        response = self.client.get(reverse('company_list'), follow=True)
+        self.assertRedirects(response, '/accounts/login/?next=/company/')
+
+    def test_company_list(self):
+        self.client.login(username='tester', password='test')
+        response = self.client.get(reverse('company_list'))
+        self.assertContains(response, 'THE MAMA AI')
